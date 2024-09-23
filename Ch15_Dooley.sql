b68430b918a0311a3764f789714d0f59bbacbd40
Imran Imtiaz <emraan.imtiaz48@gmail.com>
2024-09-23 13:10:12 +0400

    Update Ch15_Dooley.sql

--- Ch15_Dooley.sql
+++ Ch15_Dooley.sql
@@ -1,62 +1,61 @@
-/*Kyle Dooley 05/23/2023
-SQL
-Drop stored procedure if it already exists*/
-IF EXISTS (
-  SELECT * 
-    FROM INFORMATION_SCHEMA.ROUTINES 
-   WHERE SPECIFIC_SCHEMA = N'dbo.spBalanceRange'
-     AND SPECIFIC_NAME = N'dbo.spBalanceRange' 
-)
-   DROP PROCEDURE dbo.spBalanceRange
+/* Kyle Dooley 05/23/2023
+   SQL: Drop stored procedure if it already exists */
+IF OBJECT_ID(N'dbo.spBalanceRange', N'P') IS NOT NULL
+    DROP PROCEDURE dbo.spBalanceRange;
 GO
 
+/* Create the stored procedure */
 CREATE PROCEDURE dbo.spBalanceRange
-	@VendorVar  varchar(40) = '%', 
-	@BalanceMin decimal(10,2) = NULL,
-	@BalanceMax decimal(10,2) = NULL
+    @VendorVar  VARCHAR(40) = '%', 
+    @BalanceMin DECIMAL(10, 2) = NULL,
+    @BalanceMax DECIMAL(10, 2) = NULL
 AS
-	
-IF @BalanceMin IS NULL
-	SELECT @BalanceMin = MIN(InvoiceTotal) FROM Invoices
-	
-IF @BalanceMax IS NULL	
-	SELECT @BalanceMax = MAX(InvoiceTotal) FROM Invoices
+BEGIN
+    -- Set default values for @BalanceMin and @BalanceMax if they are NULL
+    IF @BalanceMin IS NULL
+        SELECT @BalanceMin = MIN(InvoiceTotal) FROM AP.dbo.Invoices;
+    
+    IF @BalanceMax IS NULL    
+        SELECT @BalanceMax = MAX(InvoiceTotal) FROM AP.dbo.Invoices;
 
-SELECT [InvoiceID]
-      ,[Invoices].[VendorID]
-      ,[InvoiceNumber]
-      ,[InvoiceDate]
-      ,[InvoiceTotal]
-      ,[PaymentTotal]
-      ,[CreditTotal]
-      ,[TermsID]
-      ,[InvoiceDueDate]
-      ,[PaymentDate]
-FROM [AP].[dbo].[Invoices]
-  	INNER JOIN Vendors ON Vendors.VendorID = Invoices.VendorID
-WHERE PaymentDate IS NULL
-	AND Vendors.VendorName LIKE @VendorVar
-	AND InvoiceTotal BETWEEN @BalanceMin AND @BalanceMax
+    -- Select invoices based on specified criteria
+    SELECT 
+        [InvoiceID],
+        [Invoices].[VendorID],
+        [InvoiceNumber],
+        [InvoiceDate],
+        [InvoiceTotal],
+        [PaymentTotal],
+        [CreditTotal],
+        [TermsID],
+        [InvoiceDueDate],
+        [PaymentDate]
+    FROM 
+        [AP].[dbo].[Invoices]
+    INNER JOIN 
+        Vendors ON Vendors.VendorID = Invoices.VendorID
+    WHERE 
+        PaymentDate IS NULL
+        AND Vendors.VendorName LIKE @VendorVar
+        AND InvoiceTotal BETWEEN @BalanceMin AND @BalanceMax;
+END;
 GO
---Test
-EXECUTE dbo.spBalanceRange @BalanceMin = 100, @BalanceMax = 2000
+
+/* Test Cases */
+USE AP;
+
+-- Test 1
+EXECUTE dbo.spBalanceRange @BalanceMin = 100, @BalanceMax = 2000;
 GO
 
---Kyle Dooley 05/23/2023
-USE AP;
+-- Test 2a
+EXECUTE dbo.spBalanceRange 'M%';
+GO
 
-EXEC spBalanceRange 'M%';
+-- Test 2b
+EXECUTE dbo.spBalanceRange @BalanceMin = 200, @BalanceMax = 1000;
+GO
 
-
-/*Kyle Dooley 05/23/2023
-Step 2b*/
-USE AP;
-
-EXEC spBalanceRange @BalanceMin = 200, @BalanceMax = 1000;
-
-
-/*Kyle Dooley 05/23/2023
-Step 2c*/
-USE AP;
-
-EXEC spBalanceRange '[C,F]%', 0, 200;
+-- Test 2c
+EXECUTE dbo.spBalanceRange '[C,F]%', 0, 200;
+GO
